a03552bab145a971f56f6ba800fe1505ab31f511
Garron Ma <garronma@gmail.com>
2019-02-05 11:18:15 -0800

    initial v1.0

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const rootDiv = document.getElementById("root");
+
+ReactDOM.render(<h1>Set up TS lintage</h1>, rootDiv);
+
+// var five = 1;
+
+// function go(nah, gah) {
+//   return null;
+// }
+
+// const hi = "hello";
+
+// const x = {
+//   val: 1,
+//   stu: 2,
+// }
